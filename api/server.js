92194d4e26ca29ae9a084895e75948c08fd87cef
Dharmik Savaliya <danny@Dharmiks-MacBook-Pro.local>
2021-09-16 12:52:24 -0700

    endpoints ready

--- api/server.js
+++ api/server.js
@@ -7,24 +7,6 @@ server.get('/', (req, res) => {
   res.status(200).json({ message: 'hey there' })
 })
 
-server.get('/api/dogs/:id', (req, res) => {
-  console.log('this is the id', req.params.id)
-  Dog.findById(req.params.id)
-    .then((dog) => {
-      console.log(dog)
-      if (dog) {
-        res.status(200).json(dog)
-      } else {
-        console.log('methings this is it')
-        res.status(404).json({ message: 'not found' })
-      }
-    })
-    .catch((err) => {
-      console.log(err)
-      res.status(500).json({ message: err.message })
-    })
-})
-
 server.get('/api/dogs', (req, res) => {
   Dog.findAll()
     .then((dogs) => {
@@ -46,19 +28,6 @@ server.post('/api/dogs', (req, res) => {
       console.log(err)
       res.status(500).json({ message: err.message })
     })
-})
-
-server.put('/api/dogs/:id', async (req, res) => {
-  const { id } = req.params
-  const changes = req.body
-  console.log(id, changes)
-  try {
-    const result = await Dog.update(id, changes)
-    res.status(200).json(result)
-  } catch (err) {
-    console.log(err)
-    res.status(500).json({ message: err.message })
-  }
 })
 
 server.delete('/api/dogs/:id', (req, res) => {
